842f61505a5b587eea33d7e32a57c2397d555a35
Amrit <amrit@scalar.com>
2025-01-23 12:21:09 -0800

    chore: testing

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -1,25 +1,66 @@
 "use client";
 
 import { ApiReferenceReact } from "@scalar/api-reference-react";
+import sluga from "sluga";
 
 export default function ApiReferencePage() {
   return (
     <ApiReferenceReact
       configuration={{
         spec: {
-          url: "https://spec.speakeasy.com/vercel/vercel-docs/vercel-oas-with-code-samples",
+          url: "https://openapi.vercel.sh/",
         },
+        hideTestRequestButton: true,
+        hideClientButton: true,
         withDefaultFonts: false,
         tagsSorter: "alpha",
-        searchHotKey: "k",
-        hideDarkModeToggle: true,
-        hideDownloadButton: true,
-        hiddenClients: true,
-        defaultHttpClient: {
-          targetKey: "shell",
-          clientKey: "curl",
-        },
         operationsSorter: "alpha",
+        searchHotKey: "l",
+        defaultOpenAllTags: false,
+        generateOperationSlug: (operation: {
+          path: string;
+          operationId: string | undefined;
+          method: string;
+          summary: string | undefined;
+        }) =>
+          `${
+            operation.summary ? sluga(operation.summary) : `${operation.path}`
+          }`,
+        hiddenClients: [
+          "libcurl",
+          "clj_http",
+          "httpclient",
+          "restsharp",
+          "jquery",
+          "native",
+          "http1.1",
+          "asynchttp",
+          "nethttp",
+          "okhttp",
+          "unirest",
+          "xhr",
+          "native",
+          "request",
+          "unirest",
+          "nsurlsession",
+          "cohttp",
+          "guzzle",
+          "http1",
+          "http2",
+          "webrequest",
+          "restmethod",
+          "requests",
+          "httr",
+          "native",
+          "httpie",
+          "wget",
+          "nsurlsession",
+          "undici",
+        ],
+        defaultHttpClient: {
+          targetKey: "js",
+          clientKey: "fetch",
+        },
       }}
     />
   );
